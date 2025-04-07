1151aa49bf43bd0b7df15684d5b29d9a684f5f35
galactic.gadgetry <galactic.gadgetry@gmail.com>
2025-04-07 14:28:57 -0400

    Add Daily Timesheet view

--- Upkeep1/ViewModels/DailyTimesheetViewModel.cs
+++ Upkeep1/ViewModels/DailyTimesheetViewModel.cs
@@ -6,9 +6,7 @@ using System.Threading.Tasks;
 
 namespace Upkeep1.ViewModels
 {
-    public class LayoutViewModel : ViewModelBase
+    public class DailyTimesheetViewModel : ViewModelBase
     {
-
-        public ViewModelBase? CurrentContentViewModel => new DailyTimesheetViewModel();
     }
 }
